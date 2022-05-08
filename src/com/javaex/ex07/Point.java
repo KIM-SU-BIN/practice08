3f6dfb4231579dc3f3ff74ead03c832ce1576f9b
subintoto <subintoto@naver.com>
2022-05-09 00:54:47 +0900

    point 결과 바꾸기 CL

--- src/com/javaex/ex07/Point.java
+++ src/com/javaex/ex07/Point.java
@@ -11,6 +11,19 @@ public class Point {
 	}
 
 	//equals() 메소드를 작성하세요.
-
-	
+	public boolean equals (Object obj) {
+		
+		/*if(!(obj instanceof PointApp))		//만약 obj 가 인스턴스오브 PointApp이 아니면 트루로 반환
+			return true;
+		PointApp p = (PointApp) obj;		//그렇지 않으면 obj를 포인트앱으로 형변환
+		return false;								//출력 오류이유는?
+		*/
+		
+		Point p = ((Point)obj);
+		if(this.x == p.x) {		
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
